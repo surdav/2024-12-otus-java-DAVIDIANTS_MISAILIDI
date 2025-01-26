8d3e76e4130cdd1a3a1a044227b4daea4b65027d
surdav <31798966+surdav@users.noreply.github.com>
2025-01-26 14:13:19 +0100

    Updated hw06-annotations

--- hw06-annotations/src/main/java/ru/otus/homework/Main.java
+++ hw06-annotations/src/main/java/ru/otus/homework/Main.java
@@ -1,7 +1,19 @@
 package ru.otus.homework;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+
+    // Logger for logging errors and information
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(final String[] args) {
-        TestRunner.runTests(MyTestClass.class);
+        // Run the tests and get the results
+        var result = TestRunner.runTests(MyTestClass.class);
+
+        // Print the results to the console
+        logger.info("--- Test Summary ---");
+        logger.info(result.toString());
     }
 }
